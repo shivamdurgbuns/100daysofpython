a1696859780d4c6f470fef65734b6789091ac24f
Shivam Durgbuns <sdurgbun@redhat.com>
2022-06-14 23:51:44 +0530

    [day07] computing problem from freecodecamp

---
+++ day07/shape_calculator.py
@@ -0,0 +1,49 @@
+class Rectangle:
+  '''
+  This is a rectangle class which will create a rectangle and its properties
+  '''
+
+  def __init__(self, width, height):
+
+    self.widht = width
+    self.height = height
+
+  def set_width(self, width):
+    self.widht = width
+
+  def set_height(self, height):
+    self.height = height
+
+  def get_area(self):
+    return (self.widht * self.height)
+
+  def get_perimeter(self):
+    return (2*(self.widht+self.height))
+
+  def get_diagonal(self):
+    return ((self.widht ** 2 + self.height ** 2) ** .5)
+
+  def get_picture(self):
+    if self.widht >= 50 or self.height >= 50:
+      return "Too big for picture."
+    
+    pic = ['*'*self.widht for i in range(self.height)]
+    return '\n'.join(pic)+'\n'
+
+  def get_amount_inside(self, shape):
+    return self.get_area()//shape.get_area()
+
+  def __str__(self):
+    if "Square" in str(self.__class__):
+      return f"Square(side={self.widht})"
+    else:
+      return f"Rectangle(width={self.widht}, height={self.height})"
+
+class Square(Rectangle):
+  
+  def __init__(self, side):
+    Rectangle.__init__(self, side, side)
+
+  def set_side(self, side):
+    self.widht = side
+    self.height = side
